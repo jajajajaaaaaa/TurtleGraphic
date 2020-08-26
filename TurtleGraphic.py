bdad3994883e0bb2903d8f3f080207808c6613c5
Mariam Siradze <51333230+mariam-siradze@users.noreply.github.com>
2020-08-26 18:46:24 +0400

    Add files via upload

---
+++ TurtleGraphic.py
@@ -0,0 +1,15 @@
+from turtle import*
+
+shape("turtle")
+
+bgcolor("#4f5f76");pencolor("#0f2862");pensize(10);fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200)
+begin_fill();fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200);end_fill()
+fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200)
+begin_fill();fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200);end_fill()
+
+
+bgcolor("#091f36");pencolor("red");pensize(6);fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100);end_fill()
+fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100)
+begin_fill();fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100);
+pencolor("red");fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100)
+
